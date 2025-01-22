da985a7ffc31c5511c868adf0d3829bc86354ec8
whoami <>
2025-01-22 21:11:53 +0800

    音乐播放

--- src/components/TracksList.tsx
+++ src/components/TracksList.tsx
@@ -1,6 +1,8 @@
+import { unknownTrackImageUri } from '@/constants/images';
 import { utilsStyles } from '@/styles';
-import { FlatList, FlatListProps, View } from "react-native";
-import { Track } from 'react-native-track-player';
+import { FlatList, FlatListProps, Text, View } from "react-native";
+import FastImage from 'react-native-fast-image';
+import TrackPlayer, { Track } from 'react-native-track-player';
 import { TrackListItem } from './TrackListItem';
 
 export type TrackListProps = Partial<FlatListProps<Track>> & {
@@ -13,9 +15,9 @@ const ItemDivider = () => (
 
 export const TracksList = ({ tracks, ...flatlistProps }: TrackListProps) => {
 
-    const handleTrackSelected = (track: Track) => {
-        console.log(track);
-
+    const handleTrackSelected = async (track: Track) => {
+        await TrackPlayer.load(track)
+        await TrackPlayer.play()
     }
     return (
         <FlatList
@@ -23,6 +25,15 @@ export const TracksList = ({ tracks, ...flatlistProps }: TrackListProps) => {
             contentContainerStyle={{ paddingTop: 10, paddingBottom: 128 }}
             ListFooterComponent={ItemDivider}
             ItemSeparatorComponent={ItemDivider}
+            ListEmptyComponent={
+                <View>
+                    <Text style={utilsStyles.emptyContentText}>No songs found</Text>
+                    <FastImage
+                        source={{ uri: unknownTrackImageUri, priority: FastImage.priority.normal }}
+                        style={utilsStyles.emptyContentImage}
+                    />
+                </View>
+            }
             renderItem={({ item: track }) => (
                 <TrackListItem track={track} onTrackSelected={handleTrackSelected} />
             )}
